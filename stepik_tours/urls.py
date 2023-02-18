b472067c7271068252ac5de39004bf121c45ab4b
ternesok <ternesok@gmail.com>
2023-02-18 21:26:59 +0300

    tour ready

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -18,9 +18,10 @@ from django.urls import path
 from tours import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.main_view),
+    path("index/", views.main_view),
     path("departure", views.departure_view),
-    path("tour/<int:tour_id>/", views.tour_view),
+    # path("tour/", views.tour_main_view),
+    path("tour/<int:tour_id>/", views.tour_view)
 ]
 # path("/departure/<str:departure>/", views.departure_view),
 # path("/tour/<int:tour_id>/", views.tour_view),
--- tours/views.py
+++ tours/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from tours import data
+from random import randint
 
 
-
-def main_view(request):
+def main_view(request, tour_id):
+    randTour = randint(1,17)
     return render(request,"index.html", context={'data': data})
 
 def departure_view(request):
     return render(request,"departure.html")
 
+# def tour_main_view(request):
+#     return render(request,"tour.html", context={'tour': data.tours})
+
 def tour_view(request, tour_id):
     return render(request,"tour.html", context={'tour': data.tours[tour_id]})
+
