b1af8b9a6f24463abf038778ddf6322d28a7b30c
ternesok <ternesok@gmail.com>
2023-02-20 07:51:40 +0300

    tour ready

--- tours/views.py
+++ tours/views.py
@@ -1,10 +1,12 @@
+import random
+
 from django.shortcuts import render
 from tours import data
 from random import randint
 
 
-def main_view(request, tour_id):
-    randTour = randint(1,17)
+def main_view(request):
+
     return render(request,"index.html", context={'data': data})
 
 def departure_view(request):
@@ -14,5 +16,7 @@ def departure_view(request):
 #     return render(request,"tour.html", context={'tour': data.tours})
 
 def tour_view(request, tour_id):
-    return render(request,"tour.html", context={'tour': data.tours[tour_id]})
+
+    cntx = {'tour': data.tours[tour_id]}
+    return render(request,"tour.html", context=cntx)
 
